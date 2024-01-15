39c4ad42f1500100e6a2aef806eb78cc62190d3f
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2024-01-15 12:41:00 -0600

    Se agregan marcadores

--- mapanimation.js
+++ mapanimation.js
@@ -1,6 +1,5 @@
-
-
-mapboxgl.accessToken =""
+const markers = [];
+mapboxgl.accessToken =  "YOUR_MAPBOX_API_TOKEN_HERE";
 
 const map = new mapboxgl.Map({
     container: "map",
@@ -9,7 +8,27 @@ const map = new mapboxgl.Map({
     zoom: 10,
 });
 
-// disable the trace start button
-function deshabilitarBoton() {
+async function run() {
+    const locations = await getBusLocations();
+    locations.forEach((location, index) => {
+        const longitude = location.attributes.longitude;
+        const latitude = location.attributes.latitude;
+        if (markers[index]) {
+            markers[index].setLngLat([longitude, latitude]);
+        } else {
+            const marker = new mapboxgl.Marker({color: "red"})
+                .setLngLat([longitude, latitude])
+                .addTo(map);
+            markers.push(marker);
+        }
+    });
+    setTimeout(run, 2000);
     document.getElementById("button").disabled = true;
+}
+
+async function getBusLocations() {
+    const url = "https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip";
+    const response = await fetch(url);
+    const json = await response.json();
+    return json.data;
 }
